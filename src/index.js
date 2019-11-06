4cced7ed7565121eda65cc5f1f44998930dc9fcb
李元秋 <yuanqiu.li@wz-inc.com>
2019-11-06 19:31:33 +0800

    add docs.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from "react";
 import ReactTHREE from "./ReactTHREE";
-import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 ReactTHREE.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
